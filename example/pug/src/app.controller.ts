9e6ceb25d6f9260f34b7db494f34111bde800c70
Audrius Raibis <audrius.raibis@gmail.com>
2025-09-07 22:54:17 +0300

    Adding pug create/delete/toggle functionality

--- example/pug/src/app.controller.ts
+++ example/pug/src/app.controller.ts
@@ -1,20 +1,66 @@
-import { Controller, Get, MessageEvent, Render } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Render, Param } from '@nestjs/common';
 import { Observable, from } from 'rxjs';
-import { PostDS, SignalsDS, DatastarService } from 'nestjs-datastar';
+import {
+  GetDS,
+  DeleteDS,
+  PostDS,
+  PatchDS,
+  SignalsDS,
+  DatastarService,
+} from 'nestjs-datastar';
 
 @Controller()
 export class AppController {
+  private readonly todos = [
+    { title: 'Learn NestJS', completed: false },
+    { title: 'Learn Datastar', completed: false },
+    { title: 'Build a Todo App', completed: false },
+  ];
   constructor(private readonly DS: DatastarService) {}
+
+  updateTodos() {
+    return from([
+      this.DS.patchElementsTemplate('todos', { todos: this.todos }),
+    ]);
+  }
 
   @Get()
   @Render('index')
   index() {}
 
-  @PostDS('merge')
-  merge(@SignalsDS() signals: Record<string, any>): Observable<MessageEvent> {
-    return from([
-      this.DS.patchElementsTemplate('merge', { word: 'Labutis' }),
-      this.DS.patchSignals(JSON.stringify({ foo: `${signals.foo} 24444` })),
-    ]);
+  @GetDS('todos')
+  getTodos(): Observable<MessageEvent> {
+    return this.updateTodos();
+  }
+
+  @PatchDS('update/:index')
+  updateTodo(
+    @Param('index') index: number,
+    @SignalsDS() signals: Record<string, any>,
+  ): Observable<MessageEvent> {
+    if (index < 0) {
+      this.todos.push({ title: String(signals?.input), completed: false });
+    } else {
+      this.todos[index] = { title: String(signals?.input), completed: false };
+    }
+    return this.updateTodos();
+  }
+
+  @DeleteDS('todo/:index')
+  deleteTodo(@Param('index') index: number): Observable<MessageEvent> {
+    this.todos.splice(index, 1);
+    return this.updateTodos();
+  }
+
+  @PostDS('toggle/:index')
+  toggleTodo(@Param('index') index: number): Observable<MessageEvent> {
+    if (index < 0) {
+      const allCompleted = this.todos.every((todo) => todo.completed);
+      this.todos.forEach((todo) => (todo.completed = !allCompleted));
+      return this.updateTodos();
+    } else {
+      this.todos[index].completed = !this.todos[index].completed;
+    }
+    return this.updateTodos();
   }
 }
--- example/pug/src/main.ts
+++ example/pug/src/main.ts
@@ -5,9 +5,9 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
-
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
